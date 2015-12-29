f8a855ce31d120f1db6a48a4e18934e6510f7e80
Jeremie BROZZU <jbrozzu@e1r10p20.42.fr>
2015-12-29 18:59:31 +0100

    gestion droit

--- framework0/db/seeds.rb
+++ framework0/db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Jeremie Brozzu",
+             email: "jbrozzu@hotmail.com",
+             password:              "coucou",
+             password_confirmation: "coucou",
+             admin: true)
+
+User.create!(name:  "Bill Cosby",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+48.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
